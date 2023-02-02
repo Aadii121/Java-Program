c5dd93b4977d3a0c6b95e8313ddc6a0cbbc01d2c
Aadii121 <officialaadi121@gmail.com>
2023-02-03 00:25:25 +0530

    Even Odd Program Done

--- Second.java
+++ Second.java
@@ -1,4 +1,12 @@
 public class Second{
-   public void m1(){
+   public static void main(String args[]){
+
+    int num=7;
+    if(num%2==0){
+    System.out.pritnln("Number is Even");
+}
+else{
+   System.out.println("Number is Odd");
+}
 }
 }
